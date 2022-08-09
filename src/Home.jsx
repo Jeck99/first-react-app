704f2606242f173ed5313a05e7e8d370e6e1afed
jeck99 <jeck99@gmail.com>
2022-08-09 14:55:31 +0300

    counter state click

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,13 +1,26 @@
-import EmployeeTable from "./Employee-Table.compnent";
-import Employees from "./Employees.component";
-function Home(props) {
-  return (
-    <div>
-      <h5> {props.fName}</h5>
-      <EmployeeTable/>
-      {/* <Employees/> */}
-    </div>
-  );
+import { Component } from "react";
+class Home extends Component {
+  constructor(props){
+    super();
+    this.state = {counter : 0};
+    this.counterUp = this.counterUp.bind(this)
+  }
+  counterUp(){
+    this.setState({counter:this.state.counter+1})
+  }
+  render() {
+    return (
+      <div>
+        <h5> {this.props.fName}</h5>
+        <button
+          onClick={this.counterUp}
+        >
+          click
+        </button>
+        <h2>{this.state.counter}</h2>
+      </div>
+    );
+  }
 }
 
 export default Home;
